7ce9b90573a41cf162108844e7d8c99926ebe887
ShotaroYoshinaga <sh-yoshinaga@sios.com>
2024-04-05 17:38:32 +0900

    WebpackからViteに移行

--- jest.config.js
+++ jest.config.js
@@ -112,9 +112,9 @@ module.exports = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "<rootDir>/src"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -140,10 +140,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -168,7 +168,7 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
   transform: {
-    "^.+\\.tsx?$": "babel-jest"
+    "^.+\\.tsx?$": "ts-jest"
   }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
